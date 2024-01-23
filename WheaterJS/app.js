8005e53ff3e9769dbb4f2237f24d5df09688f0bf
Postdie <fernandezdiego845@gmail.com>
2024-01-23 02:16:31 -0600

    segundo commit

--- WheaterJS/app.js
+++ WheaterJS/app.js
@@ -21,6 +21,47 @@ document.getElementById('miBoton').addEventListener('click', function () {
     });
 });
 
+/*document.getElementById('obtenerManual').addEventListener('click', function () {
+    console.log("Botón Obtener manual clickeado");
+    const ciudadInput = document.getElementById('ciudadInput');
+    const ciudad = ciudadInput.value;
+    if (ciudad) {
+        getManualClima(ciudad);
+    } else {
+        alert('Por favor, ingresa el nombre de la ciudad.');
+    }
+});*/
+
+document.getElementById('obtenerManual').addEventListener('click', function (e) {
+    e.preventDefault();
+    console.log("Botón Obtener manual clickeado");
+    const ciudadInput = document.getElementById('ciudadInput');
+    const ciudad = ciudadInput.value;
+
+    if (ciudad) {
+        getManualClima(ciudad);
+    } else {
+        alert('Por favor, ingresa el nombre de la ciudad.');
+    }
+
+});
+
+
+async function getManualClima(ciudad) {
+    const manualUrl = `${URLBASE}q=${ciudad}&appid=${APIKEY}`;
+
+
+    try {
+        const data = await request(manualUrl);
+        console.log("Temperatura: ", data.main.temp);
+        console.log("Ciudad: ", data.name);
+        updateDOM(data.main.temp, data.name);
+    } catch (error) {
+        console.error('Error al obtener el clima manual:', error);
+        alert('No se pudo obtener el clima para la ciudad especificada.');
+    }
+}
+
 async function updateDOM(temp, name) {
     const temperaturaElement = document.getElementById('temperatura');
     const ciudadElement = document.getElementById('ciudad');
@@ -28,17 +69,21 @@ async function updateDOM(temp, name) {
     const boton = document.getElementById('miBoton');
     const imagenContainer = document.getElementById('imagenContainer');
 
-    temperaturaElement.textContent = `Temperatura: ${temp}°C`;
+    // Convertir de Kelvin a Celsius y redondear a dos decimales
+    const tempCelsius = (temp - 273.15);
+    const tempCelsiusRounded = parseFloat(tempCelsius.toFixed(2));
+
+    temperaturaElement.textContent = `Temperatura: ${tempCelsiusRounded}°C`;
     ciudadElement.textContent = `Ciudad: ${name}`;
 
     body.classList.remove('frio', 'cool', 'calor');
     imagenContainer.innerHTML = '';
 
-    if (temp <= 10) {
+    if (tempCelsius <= 10) {
         console.log("Hace frío");
         body.classList.add('frio');
         imagenContainer.innerHTML = '<img src="./Frio.gif" alt="Hace frío">';
-    } else if (temp >= 10 && temp <= 24) {
+    } else if (tempCelsius >= 10 && tempCelsius <= 24) {
         console.log("El día está fresco");
         body.classList.add('cool');
         imagenContainer.innerHTML = '<img src="./BuenDia.gif" alt="Día fresco">';
